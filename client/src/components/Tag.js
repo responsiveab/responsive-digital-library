83fd988d534547970c7d1d029cb76ac00a5f9b64
Isak Horvath <isakhorvath@gmail.com>
2023-02-24 10:35:32 +0100

    PUM03-033: Style tag-component background with string_to_color

--- client/src/components/Tag.js
+++ client/src/components/Tag.js
@@ -5,8 +5,25 @@ import {
 } from "react-router-dom";
 
 function Tag(props) {
+
+    var string_to_color = function(str) {
+        var hash = 0;
+
+        for (var i = 0; i < str.length; i++) {
+            hash = str.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        var color = '#';
+        for (var i = 0; i < 3; i++) {
+          var value = (hash >> (i * 8)) & 0xFF;
+          color += ('00' + value.toString(16)).substr(-2);
+        }
+
+        return color;
+    }
+
     return (
-    <div className='Tag-Wrapper'>
+    <div className='Tag-Wrapper' style={{ background: string_to_color(props.content) }}>
         <Link to={"../search/tag/" + props.content}>{props.content}</Link>
     </div>);
 }
