20666cff17a974ed0235ae7d2084e3097bf6f8ee
Isak Horvath <isakhorvath@gmail.com>
2023-03-03 15:52:28 +0100

    PUM03-038: Add tags under search input field

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,31 @@
 import {Link} from 'react-router-dom';
 import {BiPlusCircle, BiUserCircle, BiSearch, BiFilter} from "react-icons/bi";
 import './css/Header.css'
+import { useState } from 'react';
+import Tag from './Tag'
+
 
 function Header() {
+    const [tags, setTags] = useState(undefined);
+
+    function appendTag(tag) {
+        if(tags) {
+            setTags(tags.concat([tag]))
+        }
+        else {
+            setTags([tag])
+        }
+    }
+
+    const handleChange = e => {
+        if(e.key == "Enter") {
+            if(e.target.value[0] == '#') {
+                appendTag(e.target.value.substring(1, e.target.value.length))
+                document.getElementById('search').value = ''
+            }
+        }
+    }
+
     return (
         <header className="App-header">
             <div style={{ float: 'left' }}>
@@ -21,9 +44,14 @@ function Header() {
             </div>
             <div className='searchbar'>
                 <BiSearch className="icon" id="search-left"/>
-                <input type="text" id="search" name="search"/>
+                <input type="text" id="search" name="search" onKeyPress={handleChange}/>
                 <BiFilter className="icon" id="search-right"/>
             </div>
+            {tags &&
+            <div className='Tag-Search-Area'>
+                {tags.map((t) => <Tag key={t} content={t} />) }
+            </div>
+             }
             <hr/>
         </header>);
 }
