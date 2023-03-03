1b8d6548ee6a7c607a3a97713fa5b325806d27b9
Isak Horvath <isakhorvath@gmail.com>
2023-03-03 11:25:33 +0100

    PUM03-049: Style login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import axios from 'axios';
+import './css/Login.css'
 
 function Login() {
     const [user, setUser] = useState({
@@ -23,12 +24,12 @@ function Login() {
 
     return (
     <main className="Login-Wrapper">
-        <p>Login</p>
+        <h3>LOGIN</h3>
         <div>
             <form action="#" autoComplete="off">
                 <input type="text" id="sign-in-email" name="email" value={user.email} onChange={handleChange} placeholder="email"/>
                 <input type="password" id="sign-in-pass" name="password" value={user.password} onChange={handleChange} placeholder="password"/>
-                <button type="button" onClick={login}>
+                <button type="button" id="sign-in-button" onClick={login}>
                     Login
                 </button>
             </form>
