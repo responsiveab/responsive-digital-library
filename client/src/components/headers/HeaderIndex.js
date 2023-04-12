c84119d6cfafc60fd0b3a620ad7f4c97d9871065
Lukas <lukmo667@student.liu.se>
2023-04-12 17:20:16 +0200

    PUM03-078: Dropdown menu and styling, not functional

--- client/src/components/headers/HeaderIndex.js
+++ client/src/components/headers/HeaderIndex.js
@@ -9,6 +9,7 @@ import Index from '../../pages/Index';
 function HeaderIndex() {
     const [tags, setTags] = useState(undefined);
     const [inputText, setInputText] = useState("");
+    const [hover,onHover] = useState(undefined);
 
     let inputHandler = (e) => {
       setInputText(e.target.value);
@@ -21,6 +22,10 @@ function HeaderIndex() {
         else {
             setTags([tag])
         }
+    }
+
+    function showMenu() {
+        
     }
 
     const handleChange = e => {
@@ -37,36 +42,50 @@ function HeaderIndex() {
             <header className="App-header">
             <div style={{ float: 'left' }}>
                 <Link to="/books/add">
-                    <BiPlusCircle className="icon"/>
+                    <BiPlusCircle className="icon"></BiPlusCircle> 
+                     
                 </Link>
             </div>
             <div style={{ float: 'right' }}>
-                <Link to="/account">
-                    <BiUserCircle className="icon"/>
-                </Link>
+                <div class="dropdown">
+                    <Link to="/account">
+                        <button class="dropbtn"><BiUserCircle className="icon"></BiUserCircle></button>
+                        <div class="item-list-wrapper">
+                            <div class="dropdown-content">
+                                <p class="dropdown-header">Hej Thomas</p>
+                                <Link to="/account">Profil</Link>
+                                <a href="#">Mina Böcker</a>
+                                <a href="#">Logga ut</a>
+                            </div>
+                        </div>
+                   </Link>
+                </div>                
                 <Link to="/">
                     {/* Placeholder for Responsive logo */}
                     <img src={process.env.PUBLIC_URL + "/logo-rdl.png"} className="img-icon"/>
-                </Link>
+                    </Link>
             </div>
             <div className='searchbar'>
-                <BiSearch className="icon" id="search-left"/>
-                {/* <input type="text" id="search" name="search" onKeyPress={handleChange}/> */}
+            <BiSearch className="icon" id="search-left"/>
+            {/* <input type="text" id="search" name="search" onKeyPress={handleChange}/> */}
                 <input 
-                  type="text" 
-                  id="search" 
-                  name="search"
-                  onChange={inputHandler}
-                  />
+                    type="text" 
+                    id="search" 
+                    name="search"
+                    onChange={inputHandler}
+                />
                 <BiFilter className="icon" id="search-right"/>
             </div>
+            
             {tags &&
             <div className='Tag-Search-Area'>
                 {tags.map((t) => <Tag key={t} content={t} />) }
             </div>
              }
+            
             <hr/>
             </header>
+            
             <div className="Index">
                 <Index input={inputText}/>
             </div>
