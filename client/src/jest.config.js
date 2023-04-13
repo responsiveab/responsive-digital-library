c4efc7a3570a3a65ddd0a80bc8f1205822aadaf8
marijennb <marijennbarcebal@gmail.com>
2023-04-13 09:03:06 +0200

    PUM03-050: Setup client test config & repo clean up.

--- client/src/jest.config.js
+++ client/src/jest.config.js
@@ -159,9 +159,9 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: [
+     "/node_modules/*"
+    ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -176,10 +176,10 @@ module.exports = {
   // transform: undefined,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+     "/node_modules/*",
+     "\\.pnp\\.[^\\/]+$"
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
