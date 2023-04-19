e4e2430fb2c78946aa5a8c8f95d3e882c62999ad
Isak Horvath <isakhorvath@gmail.com>
2023-04-19 14:51:38 +0200

    PUM03-081: Implement authentication (passward encryption, jwt-token)

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
-    password: String
+    encrypted_password: String
 })
 
 const User = mongoose.model("User", userSchema);
