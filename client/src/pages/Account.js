65528299938fd1af429b424507f3f5db8255340d
marijennb <marijennbarcebal@gmail.com>
2023-04-19 11:29:05 +0200

    PUM03-080: Added a profile page.

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,8 +1,22 @@
-import React, {useEffect} from 'react';
 import HeaderWithoutSearch from '../components/headers/HeaderWithoutSearch';
-import './css/Book.css'
+import './css/Account.css'
+import axios from 'axios';
+import React, {useEffect, useState} from 'react'
+import BookPreview from "../components/BookPreview"
+
 
 function Account(props) {
+    const [books, setBooks] = useState(undefined)
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/books/")
+          .then(res => {
+            setBooks(res.data.data)
+            console.log(res.data.data)
+          })
+          .catch(error => console.error(error));
+          // eslint-disable-next-line
+      }, []);
+
     return (
     <>
         <HeaderWithoutSearch/>
@@ -10,22 +24,51 @@ function Account(props) {
         props.user && 
         <>
             {
-             
-            <main class="Account-Wrapper">
-                <div class="Information">
-                   <div class="Photo"></div> 
-                   <h1>Profilsida för {props.user.name}</h1>
-                </div>
+                <main className="Account-Wrapper">
+                    <div className="container">
+                        <div className="flex-container">
+                            <div className="left-column">
+                                <div className="information">
+                                    <div className='Photo'>
 
-               <div class="Reading-list">
-                   <h1>Läslista för {props.user.name}</h1>
-                </div>
-                
-                <div class="Loan-list">
-                   <h1>Mina Lån</h1>
-                </div>
-                
-            </main>   
+                                    </div>
+                                    <div className='User'>
+                                        <h1>{props.user.name}</h1>
+                                        <h1>{props.user.email}</h1>
+                                    </div>
+                                    
+                               </div>
+                                <div className="reading-list">
+                                    <h1>Läslista</h1>
+                                    {books ? books.map((book) => <span key={book._id}>
+                                                            <BookPreview id={book._id} 
+                                                                          title={book.title} 
+                                                                          author={book.author}
+                                                                          body={book.body}
+                                                                          img={book.imgstr}
+                                                                          taglis={book.tags}/>
+                                                            </span>): <></>
+                                    }
+                                    
+                                </div>
+                            </div>
+                            <div className="loan-list">
+                                <h1>Mina Lån</h1>
+                                     {books ? books.map((book) => <span key={book._id}>
+                                                            <BookPreview id={book._id} 
+                                                                          img={book.imgstr}
+                                                                          
+                                                                          />
+                                                            </span>): <></>
+                                    }
+                            </div>
+                        </div>
+                    </div>
+                </main>
+
+
+             
+               
             /* 
             TODO:
             <h2>Lånade böcker</h2>
