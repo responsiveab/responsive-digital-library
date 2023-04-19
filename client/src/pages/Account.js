ba15722d54df305e58ae0f8d980dfbd80d76b093
marijennb <marijennbarcebal@gmail.com>
2023-04-19 09:58:12 +0200

    PUM03-080: Initial commit

--- client/src/pages/Account.js
+++ client/src/pages/Account.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import HeaderWithoutSearch from '../components/headers/HeaderWithoutSearch';
+import './css/Book.css'
 
 function Account(props) {
     return (
@@ -8,12 +9,29 @@ function Account(props) {
         {
         props.user && 
         <>
-            <h1>Profilsida för {props.user.name}</h1>
             {
+             
+            <main class="Account-Wrapper">
+                <div class="Information">
+                   <div class="Photo"></div> 
+                   <h1>Profilsida för {props.user.name}</h1>
+                </div>
+
+               <div class="Reading-list">
+                   <h1>Läslista för {props.user.name}</h1>
+                </div>
+                
+                <div class="Loan-list">
+                   <h1>Mina Lån</h1>
+                </div>
+                
+            </main>   
             /* 
             TODO:
             <h2>Lånade böcker</h2>
-            <h2>Favoritmarkerade böcker</h2>
+            <h2>Favoritma <div class="information">
+                <h1>Profilsida för {props.user.name}</h1>
+             </div>   rkerade böcker</h2>
             <h2>Inställningar</h2>
             */
             }
